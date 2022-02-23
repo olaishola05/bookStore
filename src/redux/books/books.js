35bd66be34016b1b8319c1f0289b805bd8e9815b
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-02-23 15:06:02 +0100

    Add action types and fetch function to book store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
 import books from '../../JS/books';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const API_KEY = '';
+const FETCH_SUCCESS = 'bookStore/books/FETCH_SUCCESS';
+const FETCH_FAIL = 'bookStore/books/FETCH_FAIL';
+const FETCH_STARTED = 'bookStore/books/FETCH_STARTED';
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -13,6 +19,35 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+const fetchStarted = () => ({
+  type: FETCH_STARTED,
+});
+
+const fetchSuccess = (books) => ({
+  type: FETCH_SUCCESS,
+  payload: books,
+});
+
+const fetchFailed = (error) => ({
+  type: FETCH_FAIL,
+  error,
+});
+
+const fetchBooks = () => async (dispatch) => {
+  dispatch(fetchStarted());
+
+  try {
+    const response = await axios({
+      method: 'get',
+      baseURL: BASE_URL,
+      url: `/apps/${API_KEY}/books`,
+    });
+    dispatch(fetchSuccess(response.data));
+  } catch (error) {
+    dispatch(fetchFailed(error.toString()));
+  }
+};
+
 const reducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK:
@@ -20,6 +55,12 @@ const reducer = (state = books, action) => {
 
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+
+    case FETCH_SUCCESS:
+      return [...action.payload];
+
+    case FETCH_FAIL:
+      return [];
 
     default:
       return state;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
